7996a4cea454e9f14721f68b3de309a9b020b1de
chandrashekhar <shekharbhoyar10@gmail.com>
2024-09-16 17:17:26 +0530

    practice

---
+++ index.js
@@ -0,0 +1,67 @@
+// // pattern 1
+for(let i=5;i>=1;i--){
+    for(let j=1;j<=i;j++){
+        document.write(j)
+    }
+   document.write("<br>")
+}
+
+// //pattern 2
+for (let i=1;i<=10;i++){
+    for(let j=1;j<=i;j++){
+        document.write(j)
+    }
+    document.write("<br>")
+}
+//pattern3
+
+// let num =(prompt('enter a number to print ascii value'));
+// let charData = string.fromCharCode(num)   
+// document.write(charData)
+
+// pattern 4
+
+for(let i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+        document.write(i)
+    }
+    document.write('<br>')
+}
+// pqttern 4
+for(let i=5;i>=1;i--){
+    for(let j=1;j<=i;j++){
+        document.write(j)}
+        document.write("<br>")
+        
+}
+ //pattern 5
+ for(let i=1;i<=5;i++){
+    for(let j=5;j>=i;j--){
+        document.write(j)
+    }
+    document.write('<br>')
+ }
+ //pattern 6
+
+ for(let i=5;i>=1;i--){
+    for(let j=1;j<=i;j++){
+        document.write(i)
+    }
+    document.write("<br>")
+ }
+
+//pattern7
+for(let i=1;i<=5;i++){
+    for(let j=5;j>=i;j--){
+        document.write(i)
+    }
+    document.write("<br>")
+ }
+
+// // pattern8
+// for(let i=5;i>=1;i){
+//     for(let j=1;j<=i;j++){
+//         document.write(i)
+//     }
+//     document.write("<br>")
+//  }
