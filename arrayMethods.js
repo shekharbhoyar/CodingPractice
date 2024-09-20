e6378a56e750094eb5bc46939bd03502c5b01391
chandrashekhar <shekharbhoyar10@gmail.com>
2024-09-20 16:11:07 +0530

    realistic problem

--- arrayMethods.js
+++ arrayMethods.js
@@ -20,6 +20,19 @@ const numbers=[4,2,8,3,0,9]
 // });
 
 // example-- print every number and also print every number multiply by 2
-numbers.forEach(function(number){
- console.log(`the number is ${number} and after multiplying by 2 the result is ${number * 2}`)
+// numbers.forEach(function(number){
+//  console.log(`the number is ${number} and after multiplying by 2 the result is ${number * 2}`)
+// })
+
+// realistic example
+const users=[
+    {firstName:"Chandu",age:20},
+    {firstName:"Nandu",age:21},
+    {firstName:"Bandhu",age:22},
+    {firstName:"Pandu",age:23},
+]
+users.forEach(function(user){
+    // console.log(user.firstName)
+    console.log(user.age)
 })
+
