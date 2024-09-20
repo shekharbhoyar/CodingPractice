9f4bb9ecea05c5cd5c981e8ed82855513f6d457a
chandrashekhar <shekharbhoyar10@gmail.com>
2024-09-20 19:36:44 +0530

    filtering out even and odd numbers

--- arrayMethods.js
+++ arrayMethods.js
@@ -46,22 +46,22 @@ const users=[
 //MAP METHOD 
  // map metod return another array
  //if you are using map function then you should always use 'return' and not'console.log'  
-const numbers=[2,8,4,0,5,4,1,]
+// const numbers=[2,8,4,0,5,4,1,]
 // const square = function(number){
 //     // console.log(number*number)
 //     return number*number
 // }
-const numSquare=numbers.map((number,index)=> {return `index${index},${number}`})
-// console.log(numSquare)
+// const numSquare=numbers.map((number,index)=> {return `index${index},${number}`})
+// // console.log(numSquare)
 
-const userss =[
-    {firstName:"Chandu",age:20},
-    {firstName:"Nandu",age:21},
-    {firstName:"Bandhu",age:22},
-    {firstName:"Pandu",age:23},
-]
-// const initial=userss.map((users)=>{return users.firstName})
-const initial=userss.map((users)=>{return users.age})
+// const userss =[
+//     {firstName:"Chandu",age:20},
+//     {firstName:"Nandu",age:21},
+//     {firstName:"Bandhu",age:22},
+//     {firstName:"Pandu",age:23},
+// ]
+// // const initial=userss.map((users)=>{return users.firstName})
+// const initial=userss.map((users)=>{return users.age})
 // console.log(initial)
 
 
@@ -71,3 +71,12 @@ const initial=userss.map((users)=>{return users.age})
 //IT TAKES CALLBACK FUNCTION AS AN INPUT
 //FILTERS CALLBACK FUNCTION MUST RETURN BOOLEAN VALUE
 //IT GIVES NEW ARRAY
+
+numbers=[2,4,1,3,7,4,9,0,3,,5,0,9,6,8]
+// const isEven= (number)=>{
+//  return number%2===0;
+// }
+const Odd=numbers.filter( (number)=>{
+    return number%2!==0;
+   })
+console.log(Odd)
